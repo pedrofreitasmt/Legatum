f4af1b1a2f59c8c00ca71073e0645cd34a54ee89
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-06-30 12:01:08 -0400

    feat: Cria tabelas de testamentos e imagens de testamentos

---
+++ database/migrations/2025_06_30_153111_create_testaments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('testaments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->longText('content');
+            $table->timestamp('send_at');
+            $table->string('recipient_email');
+            $table->boolean('is_encrypted')->default(true);
+            $table->timestamp('sent_at')->nullable();
+            $table->string('status')->default('scheduled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('testaments');
+    }
+};
