d6d652d289197c3ef41fbd5c5a8c44a6ef18d70a
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-07-01 15:12:54 -0400

    feat: adiciona campos phone e cpf na tabela users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('cpf', 11)->unique();
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
