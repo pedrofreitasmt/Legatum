49dffeb76a240528ecd6dd8f025e481f4edd536b
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-07-02 14:04:57 -0400

    fix: altera autorização para true e define regras de validação e mensagens no UpdateTestamentRequest

--- app/Http/Requests/UpdateTestamentRequest.php
+++ app/Http/Requests/UpdateTestamentRequest.php
@@ -11,7 +11,7 @@ class UpdateTestamentRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,25 @@ class UpdateTestamentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'title' => ['required', 'string', 'max:100'],
+            'content' => ['required', 'string', 'max:10000'],
+            'recipient_email' => ['required', 'email'],
+            'send_at' => ['required', 'date', 'after:today'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'O título do assunto é obrigatório.',
+            'title.max' => 'O título do assunto deve ter no máximo 100 caracteres.',
+            'content.required' => 'O conteúdo do assunto é obrigatório.',
+            'content.max' => 'O conteúdo do assunto deve ter no máximo 10000 caracteres.',
+            'recipient_email.required' => 'O email do destinatário é obrigatório.',
+            'recipient_email.email' => 'O email do destinatário deve ser um email válido.',
+            'send_at.required' => 'A data de envio é obrigatória.',
+            'send_at.date' => 'A data de envio deve ser uma data válida.',
+            'send_at.after' => 'A data de envio deve ser posterior ao dia de hoje.'
         ];
     }
 }
