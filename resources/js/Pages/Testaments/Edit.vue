c1cc726f8e21c035d034bc7397198ab839dafa36
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-07-04 11:31:57 -0400

    fix: remove campo 'send_at' do formulário de edição de testamentos

--- resources/js/Pages/Testaments/Edit.vue
+++ resources/js/Pages/Testaments/Edit.vue
@@ -11,7 +11,6 @@ const form = useForm({
     title: props.testament?.title || '',
     content: props.testament?.content || '',
     recipient_email: props.testament?.recipient_email || '',
-    send_at: props.testament?.send_at || '',
 });
 
 const submit = () => {
@@ -61,17 +60,13 @@ const goBack = () => {
                                 placeholder="Digite o email" class="bg-slate-200 text-gray-900 rounded-md w-96 py-2">
                             <InputError :message="form.errors.recipient_email" class="mt-2"></InputError>
                         </div>
-
-                        <div class="flex flex-col gap-2 items-center">
-                            <label class="font-semibold" for="send_at">Atualizar data de envio</label>
-                            <input v-model="form.send_at" name="send_at" type="date"
-                                class="bg-slate-200 text-gray-900 rounded-md px-5 font-semibold">
-                            <InputError :message="form.errors.send_at" class="mt-2"></InputError>
-                        </div>
                     </div>
                     <div class="flex justify-center mt-6 gap-2">
-                        <button class="bg-green-500 hover:bg-green-600  text-gray-50 rounded-full font-semibold px-6 py-2 cursor-pointer" type="submit">Atualizar Testamento</button>
-                        <button @click="goBack" type="button" class="bg-yellow-500 hover:bg-yellow-600 text-gray-50 rounded-full font-semibold px-4 py-2 cursor-pointer">Voltar</button>
+                        <button
+                            class="bg-green-500 hover:bg-green-600  text-gray-50 rounded-full font-semibold px-6 py-2 cursor-pointer"
+                            type="submit">Atualizar Testamento</button>
+                        <button @click="goBack" type="button"
+                            class="bg-yellow-500 hover:bg-yellow-600 text-gray-50 rounded-full font-semibold px-4 py-2 cursor-pointer">Voltar</button>
                     </div>
                 </form>
             </div>
