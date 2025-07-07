954fb3cc7114362af164daffade6e1a55a55e6b8
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-07-07 16:54:22 -0400

    feat: adicionar suporte para anexos ao criar testamentos

--- app/Actions/StoreTestamentAction.php
+++ app/Actions/StoreTestamentAction.php
@@ -4,6 +4,8 @@ namespace App\Actions;
 
 use App\Http\Requests\StoreTestamentRequest;
 use App\Models\Testament;
+use Illuminate\Support\Arr;
+
 
 class StoreTestamentAction
 {
@@ -17,6 +19,19 @@ class StoreTestamentAction
 
         $validatedData['user_id'] = auth()->id();
 
-        Testament::create($validatedData);
+        $testament = Testament::create(Arr::except($validatedData, ['attachments']));
+
+        if ($request->hasFile('attachments')) {
+            foreach ($request->file('attachments') as $file) {
+                $path = $file->store('attachments', 'public');
+
+                $testament->testamentAttachments()->create([
+                    'path' => $path,
+                    'original_name' => $file->getClientOriginalName(),
+                    'mime_type' => $file->getClientMimeType(),
+                    'size' => $file->getSize(),
+                ]);
+            }
+        }
     }
 }
