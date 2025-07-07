78736a254027075c606a40fd447768b74ef648c3
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-07-07 16:57:12 -0400

    refactor: ajustar o método testaments para tipagem explícita e remover método não utilizado

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
@@ -67,17 +68,8 @@ class User extends Authenticatable
         ];
     }
 
-    public function testaments()
+    public function testaments(): HasMany
     {
         return $this->hasMany(Testament::class);
-    }
-
-    public function getTestamentsByDate(): User
-    {
-        return auth()->user()->load([
-            'testaments' => function ($query): void {
-                $query->orderByDesc('created_at');
-            },
-        ]);
     }
 }
