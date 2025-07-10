2ee4dcfd576fd8f85b234a39b0a7d1eeb8594474
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-07-10 17:25:21 -0400

    feat: adicionar 'is_admin' ao preenchimento em massa e ao casting do modelo User

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,6 +32,7 @@ class User extends Authenticatable
         'password',
         'phone',
         'cpf',
+        'is_admin',
     ];
 
     /**
@@ -65,6 +66,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_admin' => 'boolean',
         ];
     }
 
