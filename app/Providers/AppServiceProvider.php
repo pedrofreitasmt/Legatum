42d1b9cc36870c4f23bf06b386add6dfb86df6d3
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-07-10 17:25:39 -0400

    feat: definir autorização para acesso ao painel administrativo com base no status de administrador do usuário

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::automaticallyEagerLoadRelationships();
+
+        Gate::define('viewAdminPanel', function ($user) {
+            return $user->is_admin;
+        });
     }
 }
