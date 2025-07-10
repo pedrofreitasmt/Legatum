0437c8e654385a560327ca08cdaf92db006e87e7
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-07-10 17:27:57 -0400

    feat: criar páginas de Usuários e Detalhes do Usuário

---
+++ resources/js/Pages/Users/Index.vue
@@ -0,0 +1,15 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import { Link } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+    <AppLayout title="Usuários">
+        <template #header>
+            <h2>
+
+            </h2>
+        </template>
+    </AppLayout>
+</template>
---
+++ resources/js/Pages/Users/Show.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+
+</script>
+
+<template>
+    <AppLayout title="Detalhes do Usuário">
+        <template #header>
+            <h2>
+                Detalhes do Usuário
+            </h2>
+        </template>
+    </AppLayout>
+</template>
