d5a288c8c7bd3324d05be061894f7583b0d02923
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-07-17 08:40:47 -0400

    feat: implementar lógica de erro 404 nas funções de controle de usuário e carregar testamentos

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -18,31 +18,33 @@ class UserController extends Controller
 
     public function create()
     {
-        //
+        abort(404);
     }
 
     public function store(Request $request)
     {
-        //
+        abort(404);
     }
 
     public function show(User $user): Response
     {
+        $user->loadCount('testaments')->load('testaments');
+
         return Inertia::render('Users/Show', compact('user'));
     }
 
     public function edit(string $id)
-    {
-        //
+    { 
+        abort(404);
     }
 
     public function update(Request $request, string $id)
     {
-        //
+        abort(404);
     }
 
     public function destroy(string $id)
     {
-        //
+        abort(404);
     }
 }
