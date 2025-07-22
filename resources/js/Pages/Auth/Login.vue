b605d065fbd1f6388c2ea2b62d0b9b7f1f4da5dd
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-07-22 09:13:38 -0400

    refactor: traduzir rótulo de senha para português

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -57,7 +57,7 @@ const submit = () => {
             </div>
 
             <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+                <InputLabel for="password" value="Senha" />
                 <TextInput
                     id="password"
                     v-model="form.password"
