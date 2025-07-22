64de5e4dd7f67ee61c9315c1108d5b07b54f55a5
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-07-22 09:33:49 -0400

    feat: adicionar classe DecryptContentCast para manipulação de atributos criptografados

---
+++ app/Casts/DecryptContentCast.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
+
+class DecryptContentCast implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function get(Model $model, string $key, mixed $value, array $attributes): mixed
+    {
+        if (isset($attributes['is_encrypted']) && $attributes['is_encrypted'] && !is_null($value)) {
+            try {
+                return Crypt::decrypt($value);
+            } catch (DecryptException $e) {
+                return $value;
+            }
+        }
+
+        return $value;
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  array<string, mixed>  $attributes
+     */
+    public function set(Model $model, string $key, mixed $value, array $attributes): mixed
+    {
+        return $value;
+    }
+}
