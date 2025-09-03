5635918d6d566435b0cd3bc66570bb2ac9f85a20
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-09-03 14:52:08 -0400

    feat: adicionar métodos de configuração para comandos, modelos e Vite no AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +28,28 @@ class AppServiceProvider extends ServiceProvider
         Gate::define('viewAdminPanel', function ($user) {
             return $user->is_admin;
         });
+
+        $this->configureCommands();
+        $this->configureModels();
+        $this->configureVite();
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->environment('production'),
+        );
+    }
+
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict();
+
+        Model::unguard();
+    }
+
+    private function configureVite(): void
+    {
+        Vite::usePrefetchStrategy('agressive');
     }
 }
