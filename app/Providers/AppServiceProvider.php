54211b67be95028677571cf142d01da68abd9a69
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-09-03 14:52:21 -0400

    refactor: remover comentários desnecessários do AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,17 +10,11 @@ use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         Model::automaticallyEagerLoadRelationships();
