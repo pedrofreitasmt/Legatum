0bdb09569030379d74ed721912713ef7a827464b
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-09-03 14:52:29 -0400

    feat: adicionar configuração de usuários administrativos e comuns no TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,25 @@
 
 namespace Tests;
 
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use RefreshDatabase;
+
+    protected User $adminUser;
+    protected User $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->adminUser = User::factory([
+            'is_admin' => true,
+        ])->create();
+
+        $this->user = User::factory()->create();
+    }
 }
