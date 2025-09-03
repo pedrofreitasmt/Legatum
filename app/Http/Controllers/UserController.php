4ae57216e42036ac0c2c467c3c9f68ef4fbf1f22
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-09-03 14:50:45 -0400

    refactor: remover métodos não utilizados do UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -16,35 +15,10 @@ class UserController extends Controller
         return Inertia::render('Users/Index', compact('users'));
     }
 
-    public function create()
-    {
-        abort(404);
-    }
-
-    public function store(Request $request)
-    {
-        abort(404);
-    }
-
     public function show(User $user): Response
     {
         $user->loadCount('testaments')->load('testaments');
 
         return Inertia::render('Users/Show', compact('user'));
-    }
-
-    public function edit(string $id)
-    { 
-        abort(404);
-    }
-
-    public function update(Request $request, string $id)
-    {
-        abort(404);
-    }
-
-    public function destroy(string $id)
-    {
-        abort(404);
     }
 }
