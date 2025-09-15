17e836510eaddeabf1a274f56f8079a218f5affe
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-09-15 17:39:05 -0400

    feat: adicionar verificação de permissão para Super Admin no Gate

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        Gate::before(function ($user, $ability) {
+            return $user->hasRole('Super Admin') ? true : null;
+        });
+
         Model::automaticallyEagerLoadRelationships();
 
         Gate::define('viewAdminPanel', function ($user) {
