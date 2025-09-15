2a6efcc82203c4d77128897300ef403d97b771f3
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-09-15 17:39:59 -0400

    feat: criar seeder para papéis e permissões

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    public function run(): void
+    {
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $actions = [
+            'listar',
+            'criar',
+            'editar',
+            'detalhar',
+            'apagar',
+            'trocar status',
+            'relatorio',
+        ];
+
+        $permissions = [
+           //
+        ];
+
+        $arrayOfPermissionNames = ['default'];
+
+        foreach ($permissions as $permission) {
+            foreach ($actions as $action) {
+                $arrayOfPermissionNames[] = sprintf('%s %s', $permission, $action);
+            }
+        }
+
+        $permissions = collect($arrayOfPermissionNames)->map(function ($permission): array {
+            return ['name' => $permission, 'guard_name' => 'web'];
+        });
+
+        $permissionsCreated = Permission::pluck('name');
+
+        $permissions = $permissions->filter(function ($permission) use ($permissionsCreated) {
+            return !$permissionsCreated->contains($permission['name']);
+        });
+
+        if ($permissions->isEmpty()) {
+            return;
+        }
+
+        $permissions = $permissions->map(function ($permission) {
+            return Permission::create($permission);
+        });
+
+        $roles = Role::pluck('name');
+
+        if ($roles->contains('Super Admin')) {
+            return;
+        }
+
+        Role::create(['name' => 'Super Admin']);
+    }
+}
