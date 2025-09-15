48d14c1a5e5c3b510c3e9e7c06dc2b12a428be4a
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-09-15 17:39:51 -0400

    feat: adicionar chamada para RolesAndPermissionsSeeder no DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,6 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            RolesAndPermissionsSeeder::class,
             UserSeeder::class,
             TestamentSeeder::class,
         ]);
