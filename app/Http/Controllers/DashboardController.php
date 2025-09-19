e25c36ac74e978ec3b64ec31799049f75ece2a82
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-09-19 15:54:41 -0400

    feat: criar o controlador DashboardController para renderizar a página do dashboard

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class DashboardController extends Controller
+{
+    public function __invoke(Request $request): Response
+    {
+        $user = auth()->user();
+
+        return Inertia::render('Dashboard', compact('user'));
+    }
+}
