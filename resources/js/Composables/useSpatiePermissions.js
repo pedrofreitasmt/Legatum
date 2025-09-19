3e27ad17931124cc371a9731a569a56ce0fe009d
Pedro Freitas <pedrohenrique_of@hotmail.com>
2025-09-19 17:18:48 -0400

    refatorar a indentação e o espaçamento no arquivo useSpatiePermissions.js

--- resources/js/Composables/useSpatiePermissions.js
+++ resources/js/Composables/useSpatiePermissions.js
@@ -1,19 +1,19 @@
 import { usePage } from '@inertiajs/vue3'
 
 export function useSpatiePermissions() {
-  const page = usePage()
+    const page = usePage()
 
-  const hasPermission = (permission) => {
-    return page.props.auth.permissions.includes(permission)
-  }
+    const hasPermission = (permission) => {
+        return page.props.auth.permissions.includes(permission)
+    }
 
-  const hasAnyPermission = (permissions) => {
-    return permissions.some(p => hasPermission(p))
-  }
+    const hasAnyPermission = (permissions) => {
+        return permissions.some(p => hasPermission(p))
+    }
 
-  const hasAllPermissions = (permissions) => {
-    return permissions.every(p => hasPermission(p))
-  }
+    const hasAllPermissions = (permissions) => {
+        return permissions.every(p => hasPermission(p))
+    }
 
-  return { hasPermission, hasAnyPermission, hasAllPermissions }
+    return { hasPermission, hasAnyPermission, hasAllPermissions }
 }
